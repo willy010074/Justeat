c640f6ab33eb8807c1eeec436c5d4f1bc7b422d8
1033025 <willy010074@gmail.com>
2017-06-18 00:42:50 +0800

    隨機產生 : done 可以選擇類別 新增店家 : bug 查看全部 : done

--- app/src/main/java/com/example/chungyu/justeat/MainActivity.java
+++ app/src/main/java/com/example/chungyu/justeat/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.chungyu.justeat;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,10 +26,11 @@ public class MainActivity extends AppCompatActivity implements ChildEventListene
     TextView txv;
     Random random;
     int idx;
-    int len;
-    ArrayList<String> result ;
-    DatabaseReference myRef;
+    int flag;
+
     Toast tos;
+    Spinner time_sp;
+    FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +38,25 @@ public class MainActivity extends AppCompatActivity implements ChildEventListene
         setContentView(R.layout.activity_main);
 
         txv = (TextView) findViewById(R.id.txv);
-        result = new ArrayList<String>();
-        result.clear();
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        myRef = db.getReference("breakfast");
-        myRef.addChildEventListener(this);
+        db = FirebaseDatabase.getInstance();
         tos = Toast.makeText(this,"",Toast.LENGTH_SHORT);
+        time_sp = (Spinner) findViewById(R.id.spinner_time);
+
+        /*flag = 0;
+        myBreakFast = db.getReference("breakfast");
+        myBreakFast.addChildEventListener(this);*/
+
+        /*flag = 1;
+        myBrunch = db.getReference("brunch");
+        myBrunch.addChildEventListener(this);
+
+        /*flag = 2;
+        myLunch = db.getReference("lunch");
+        myLunch.addChildEventListener(this);
+
+        flag = 3;
+        myDinner = db.getReference("dinner");
+        myDinner.addChildEventListener(this);*/
     }
 
     public void gotolist(View v)
@@ -59,11 +69,102 @@ public class MainActivity extends AppCompatActivity implements ChildEventListene
         random = new Random();
         idx = random.nextInt(100000);
 
-        idx = idx % result.size();
-        txv.setText(result.get(idx));
+        String[] times = getResources().getStringArray(R.array.time);
+        int index = time_sp.getSelectedItemPosition();
+        switch (index) {
+            case 0:
+                final ArrayList<String> resultBreakfast = new ArrayList<String>();
+                DatabaseReference db_breakfast = FirebaseDatabase.getInstance().getReference("breakfast");
+                db_breakfast.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        resultBreakfast.clear();
+                        for (DataSnapshot ds : dataSnapshot.getChildren() ){
+                            resultBreakfast.add(ds.child("name").getValue().toString());
+                        }
+
+                        idx = idx % resultBreakfast.size();
+                        txv.setText(resultBreakfast.get(idx));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                break;
+
+            case 1:
+                final ArrayList<String> result_Branch = new ArrayList<String>();
+                DatabaseReference db_brunch = FirebaseDatabase.getInstance().getReference("brunch");
+                db_brunch.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        result_Branch.clear();
+                        for (DataSnapshot ds : dataSnapshot.getChildren() ){
+                            result_Branch.add(ds.child("name").getValue().toString());
+                        }
+
+                        idx = idx % result_Branch.size();
+                        txv.setText(result_Branch.get(idx));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                break;
+
+            case 2:
+                final ArrayList<String> result_Lunch = new ArrayList<String>();
+                DatabaseReference db_lunch = FirebaseDatabase.getInstance().getReference("lunch");
+                db_lunch.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        result_Lunch.clear();
+                        for (DataSnapshot ds : dataSnapshot.getChildren() ){
+                            result_Lunch.add(ds.child("name").getValue().toString());
+                        }
+
+                        idx = idx % result_Lunch.size();
+                        txv.setText(result_Lunch.get(idx));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+
+            case 3:
+                final ArrayList<String> result_Dinner = new ArrayList<String>();
+                DatabaseReference db_dinner = FirebaseDatabase.getInstance().getReference("dinner");
+                db_dinner.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        result_Dinner.clear();
+                        for (DataSnapshot ds : dataSnapshot.getChildren() ){
+                            result_Dinner.add(ds.child("name").getValue().toString());
+                        }
+
+                        idx = idx % result_Dinner.size();
+                        txv.setText(result_Dinner.get(idx));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+        }
     }
 
-    public void push(View V){
+    /*public void push(View V){
         //LayoutInflater inflater = this.getLayoutInflater();
         final View view = View.inflate(this,R.layout.my_dialog,null);
         final EditText text_name = (EditText) view.findViewById(R.id.name);
@@ -71,8 +172,8 @@ public class MainActivity extends AppCompatActivity implements ChildEventListene
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("請輸入店名及地址").setView(view).setPositiveButton("Ok",new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog,int id){
-                myRef.child(String.valueOf(result.size()+1)).child("name").setValue(text_name.getText().toString());
-                myRef.child(String.valueOf(result.size()+1)).child("addr").setValue(text_addr.getText().toString());
+                myBreakFast.child(String.valueOf(result_Breakfast.size()+1)).child("name").setValue(text_name.getText().toString());
+                myBreakFast.child(String.valueOf(result_Breakfast.size()+1)).child("addr").setValue(text_addr.getText().toString());
                 tos.setText("成功新增"+text_name.getText().toString()+"!!");
                 tos.show();
             }
@@ -83,13 +184,32 @@ public class MainActivity extends AppCompatActivity implements ChildEventListene
         });
         AlertDialog dialog = builder.create();
         dialog.show();
-    }
+    }*/
 
 
 
     @Override
     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-        result.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+        //result.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+        /*txv.setText(Integer.toString(flag));
+        switch (flag) {
+            case 0:
+                result_Breakfast.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+                break;
+
+            case 1:
+                result_Brunch.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+                break;
+
+            case 2:
+                result_Lunch.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+                break;
+
+            case 3:
+                result_Dinner.add(String.valueOf(dataSnapshot.child("name").getValue().toString()));
+                break;
+        }*/
+
     }
 
     @Override
